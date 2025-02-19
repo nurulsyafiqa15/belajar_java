cf3c6d2d27f0e90d1bd9d108ab44bb4701ccc3ae
nurulsyafiqa15 <nurulsyafiqa1509@gmail.com>
2025-02-19 15:15:34 +0700

    Latihan1nurulsyafiqaramadhan

---
+++ Latihan1.java
@@ -0,0 +1,14 @@
+package bin.com.belajar;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        String nama = "nurul";
+        String kelas = " X PPLG 2";
+        double B_badan = 35.00;
+        System.out.println("Nama saya : " +nama);
+        System.err.println("kelas : " +kelas);
+        System.err.print("Berat Badan : ");
+        System.out.println(B_badan);
+    }
+    
+}
