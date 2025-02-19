b2fbfbcefc71861dcfc119f978579d7b23968857
nurulsyafiqa15 <nurulsyafiqa1509@gmail.com>
2025-02-19 15:16:14 +0700

    Add files via upload

---
+++ Latihan2.java
@@ -0,0 +1,16 @@
+package bin.com.belajar;
+
+public class Latihan2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 30;
+        System.out.println("Hasil a + b = " + (a + b));
+        System.out.println("Hasil a - b = " + (a - b));
+        System.out.println("Hasil a * b = " + (a * b));
+        System.out.println("Hasil b / a = " + (b / a));
+        System.out.println("Hasil c % a = " + (c % a));
+        System.out.println("Hasil d % c = " + (d % b));
+    }
+}
